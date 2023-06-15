56d389a0c03b53481c765e701396b16dcc60df59
TianYuan <white-sky@qq.com>
2023-06-15 03:23:38 +0000

    update start.sh of Encodec*

--- examples/Encodec_24k_32d/start.sh
+++ examples/Encodec_24k_32d/start.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 source path.sh
+log_root=logs
+# 24kHz *.wav in train_data_dir
+train_data_dir=dump/train
+valid_data_dir=dump/valid
 
 python3 ${BIN_DIR}/main3_ddp.py \
         --BATCH_SIZE 16 \
         --N_EPOCHS 300 \
-        --save_dir path_to_save_log \
-        --PATH  path_to_save_model \
-        --train_data_path path_to_training_data \
-        --valid_data_path path_to_val_data \
+        --save_dir ${log_root} \
+        --PATH ${log_root} \
+        --train_data_path ${train_data_dir} \
+        --valid_data_path ${valid_data_dir} \
         --sr 24000 \
         --ratios 2 2 2 4 \
         --target_bandwidths 7.5 15
